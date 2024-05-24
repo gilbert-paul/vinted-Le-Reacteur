2ccfc7feb385b77e1ee22b16fc66a59b6639648f
Paul <paul.gilbert.1996@gmail.com>
2024-05-24 13:41:08 +0200

    Modifications .env

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI)
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_SECRET_KEY,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET,
 });
 
 app.use(cors())
