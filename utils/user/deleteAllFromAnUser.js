9cd42eccee8c69faad341f6189d90b7a2637a088
Paul <paul.gilbert.1996@gmail.com>
2024-05-24 19:27:08 +0200

    Modification suite à issue

--- utils/user/deleteAllFromAnUser.js
+++ utils/user/deleteAllFromAnUser.js
@@ -3,17 +3,18 @@ const User = require("../../models/User");
 const cloudinary = require("cloudinary").v2;
 
 const deleteAllFromAnUser = async (req, res) => {
-  const thisUser = await User.findById(req.user._id);
+  const thisUser = await User.findById(req.user._id).populate("avatar");
   if (!thisUser) {
     return res.status(404).json({ message: "Id is invalid" });
   }
   const allOffers = await Offer.find({ owner: thisUser._id });
+  console.log("a")
   if (allOffers.length > 0) {
     for (let i = 0; i < allOffers.length; i++) {
-
-      //
       const thisOffer = allOffers[i]
       if (thisOffer) {      
+  console.log("b")
+
         if (thisOffer.product_pictures.length > 0) {
           await cloudinary.api.delete_resources_by_prefix(
             thisOffer.product_pictures[0].folder
@@ -37,14 +38,21 @@ const deleteAllFromAnUser = async (req, res) => {
         //
     }
   }
+  console.log("c")
 
   if (thisUser.account.avatar.public_id) {
     await cloudinary.api.delete_resources(thisUser.account.avatar.public_id);
   }
+  console.log("d")
+
   if (thisUser.account.avatar.folder) {
     await cloudinary.api.delete_folder(thisUser.account.avatar.folder);
   }
+  console.log("e")
+
   await User.findByIdAndDelete(req.user._id);
+  console.log("f")
+
   return res.status(202).json({ message: "User and Offers deleted" });
 };
 
