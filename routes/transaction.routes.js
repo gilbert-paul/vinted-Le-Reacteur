04a232a84bff8e0036405f93b4de316b575ac262
Paul <paul.gilbert.1996@gmail.com>
2024-06-18 20:17:47 +0200

    upload sell & buy routes

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -12,11 +12,15 @@ const Transaction = require("../models/Transaction.js");
 router.get("/buy", isAuthentificated, async (req, res) => {
   try {
     
-    const result = {
-      status:200,
-      message:"buy",
-      data:{}
-    }
+      const user = req.user
+      const sellTransactions = await Transaction.find({buyer:{_id:user._id}}).populate("seller buyer offer")
+      console.log(sellTransactions)
+      const result = {
+        status:200,
+        message:"sell",
+        data:{sellTransactions}
+      }
+
    return res.status(result.status).json({message: result.message, data: result.data});
   } catch (error) {
     return res.status(500).json({ message: "Error with BDD" });
@@ -25,7 +29,8 @@ router.get("/buy", isAuthentificated, async (req, res) => {
 router.get("/sell", isAuthentificated, async (req, res) => {
   try {
     const user = req.user
-    const sellTransactions = await Transaction.find({seller._id:user._id})
+    const sellTransactions = await Transaction.find({seller:{_id:user._id}}).populate("seller buyer offer")
+    console.log(sellTransactions)
     const result = {
       status:200,
       message:"sell",
